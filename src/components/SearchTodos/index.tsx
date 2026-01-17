852845fdecce4e97d6ee7a60673a06b65c1d7448
Eugenech1988 <eugenech1988@gmail.com>
2026-01-17 15:06:20 +0200

    search todos refactor

--- src/components/SearchTodos/index.tsx
+++ src/components/SearchTodos/index.tsx
@@ -1,5 +1,5 @@
 import { useTodo } from '@/context/todoContext';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { cn, debounce } from '@/utils';
 import { Search } from 'lucide-react';
 import { FilterDropdown } from '@/components/FilterDropdown';
@@ -7,19 +7,18 @@ import { FilterDropdown } from '@/components/FilterDropdown';
 export const SearchTodos = () => {
   const { setSearchQuery } = useTodo();
   const inputRef = useRef<HTMLInputElement>(null);
-  const [inputValue, setInputValue] = useState('');
 
   const debouncedSetSearchQuery = useRef(
     debounce(() => {
+      // Берем значение напрямую из рефа
       if (inputRef.current) {
         setSearchQuery(inputRef.current.value);
       }
     }, 300)
-  );
+  ).current;
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-    debouncedSetSearchQuery.current();
+  const handleChange = () => {
+    debouncedSetSearchQuery();
   };
 
   return (
@@ -29,31 +28,31 @@ export const SearchTodos = () => {
           ref={inputRef}
           type="text"
           id="search-todo-input"
-          value={inputValue}
           onChange={handleChange}
           placeholder=" "
-          required
           className={cn(
             'peer w-full px-4 leading-[42px] pl-10 border rounded-lg transition-all',
             'border-gray-300 focus:border-blue-500 focus:outline-none',
-            'invalid:animate-shake',
-            inputValue ? 'border-blue-500' : ''
+            'peer-[:not(:placeholder-shown)]:border-blue-500'
           )}
         />
+
         <Search
           className={cn(
-            'absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 pointer-events-none transition-colors duration-200',
-            'peer-placeholder-shown:text-gray-400 peer-focus:text-blue-500 peer-not-placeholder-shown:text-blue-500',
-            inputValue ? 'text-blue-500' : ''
+            'absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 pointer-events-none transition-colors duration-200 text-gray-400',
+            'peer-focus:text-blue-500',
+            'peer-[:not(:placeholder-shown)]:text-blue-500'
           )}
         />
+
         <label
           htmlFor="search-todo-input"
           className={cn(
-            'absolute left-3 -top-2.5 bg-white px-1 text-sm transition-all',
-            'peer-placeholder-shown:top-2 peer-placeholder-shown:left-10 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400',
+            'absolute transition-all bg-white px-1 pointer-events-none cursor-text',
+            'left-10 top-2 text-base text-gray-400',
+            'peer-placeholder-shown:top-2 peer-placeholder-shown:left-10 peer-placeholder-shown:text-base',
             'peer-focus:-top-2.5 peer-focus:left-3 peer-focus:text-sm peer-focus:text-blue-500',
-            inputValue ? '-top-2.5 left-3 text-sm text-blue-500' : ''
+            'peer-[:not(:placeholder-shown)]:-top-2.5 peer-[:not(:placeholder-shown)]:left-3 peer-[:not(:placeholder-shown)]:text-sm peer-[:not(:placeholder-shown)]:text-blue-500'
           )}
         >
           Search todos
